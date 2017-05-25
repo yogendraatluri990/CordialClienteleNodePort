c23e28303907b15db73ce3fa7d66c61e62d51892
yogendra.atluri <yogendra.atluri@gmail.com>
2017-05-25 11:48:20 -0700

    Created a simple node app

--- cordialclienteleNodeApp.js
+++ cordialclienteleNodeApp.js
@@ -1,2 +1,17 @@
 var express = require('express');
 var cordialApp = express();
+var fs = require('fs');
+var bodyParser = require('body-parser');
+var cookiParser = require('cookie-parser');
+
+var port = process.env.PORT || 3030;
+
+// Creating a Public container to hold all the necessary files
+cordialApp.use('/cordialAssets',express.static(__dirname + '/public'));
+
+cordialApp.get('/',(req,res)=>{
+    console.log('Welcome to the CordialClientele Node Port');
+});
+
+
+cordialApp.listen(port);
